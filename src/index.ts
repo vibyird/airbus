0dcf0061005209ba6fa1fc566a30bd00049d5f0b
Vibyird <vibyird@gmail.com>
2025-12-01 13:25:56 +0800

    [RC]调整favicon.ico

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import { httpServerHandler } from 'cloudflare:node'
 import express from 'express'
+import favicon from './assets/favicon.ico'
 import index from './assets/index.html'
 import subscribe from './routes/subscribe'
 
 const app = express()
 
-app.use('/subscribe', subscribe)
-
 app.get('/', async (req, res) => {
   res.send(index)
 })
+
+app.get('/favicon.ico', async (req, res) => {
+  res.setHeader('Content-Type', 'image/x-icon')
+  res.send(Buffer.from(favicon))
+})
+
+app.use('/subscribe', subscribe)
 
 app.use(async (req, res) => {
   res.status(404).send('Not Found')
