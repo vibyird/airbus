b6da6a2d2af97e31fde44ea37846924bd8680dc3
Vibyird <vibyird@gmail.com>
2025-12-05 10:34:05 +0800

    [RC]简化逻辑

--- src/index.ts
+++ src/index.ts
@@ -5,22 +5,11 @@ import subscribe from './routes/subscribe'
 
 const router = new Router()
 
-router.use('/subscribe', subscribe.routes())
+router.use('/subscribe', subscribe.routes(), subscribe.allowedMethods())
 
 const app = new App()
 
 app.use(router.routes()).use(router.allowedMethods())
-
-app.use(async (ctx) => {
-  if (ctx.status === 404) {
-    ctx.set('Content-Type', 'text/plain; charset=utf-8')
-    ctx.body = 'Not Found'
-  }
-  if (ctx.status === 500) {
-    ctx.set('Content-Type', 'text/plain; charset=utf-8')
-    ctx.body = 'Server Error'
-  }
-})
 
 app.listen(3000)
 export default httpServerHandler({ port: 3000 })
--- src/routes/subscribe.ts
+++ src/routes/subscribe.ts
@@ -6,49 +6,42 @@ const router = new Router()
 router.get('/:token', async (ctx) => {
   const token = ctx.params.token
   if (!token) {
-    ctx.status = 404
+    ctx.throw(404)
     return
   }
 
-  try {
-    const subscriber = await findSubscriber(token)
-    if (!subscriber) {
-      ctx.status = 404
-      return
-    }
-    const directDomains = []
-    if (process.env.DIRECT_DOMAINS) {
-      directDomains.push(
-        ...process.env.DIRECT_DOMAINS.split(',')
-          .map((domain) => domain.trim())
-          .filter(Boolean),
-      )
-    }
-
-    const { subscribeName, subscribeUrl } = subscriber
-    directDomains.push(...subscriber.directDomains)
-
-    const userAgent = ctx.get('user-agent')
-    if (/clash/i.test(userAgent) || /stash/i.test(userAgent)) {
-      ctx.set({
-        'Content-Type': 'application/x-yaml; charset=utf-8',
-        'Content-Disposition': `attachment; filename=${subscribeName}.yaml`,
-      })
-      ctx.body = clashConfig
-        .replace(/\${subscribeName}/g, subscribeName)
-        .replace(/\${subscribeUrl}/g, subscribeUrl)
-        .replace(
-          /([^\r\n]*)\$\{directDomain\}([^\r\n]*)(\r?\n)/m,
-          directDomains.map((directDomain) => `$1${directDomain}$2$3`).join(''),
-        )
-      return
-    } else {
-      ctx.status = 404
-      return
-    }
-  } catch (error) {
-    ctx.status = 500
+  const subscriber = await findSubscriber(token)
+  if (!subscriber) {
+    ctx.throw(404)
+    return
   }
+  const directDomains = []
+  if (process.env.DIRECT_DOMAINS) {
+    directDomains.push(
+      ...process.env.DIRECT_DOMAINS.split(',')
+        .map((domain) => domain.trim())
+        .filter(Boolean),
+    )
+  }
+  const { subscribeName, subscribeUrl } = subscriber
+  directDomains.push(...subscriber.directDomains)
+
+  const userAgent = ctx.get('user-agent')
+  if (/clash/i.test(userAgent) || /stash/i.test(userAgent)) {
+    ctx.set({
+      'Content-Type': 'application/x-yaml; charset=utf-8',
+      'Content-Disposition': `attachment; filename=${subscribeName}.yaml`,
+    })
+    ctx.body = clashConfig
+      .replace(/\${subscribeName}/g, subscribeName)
+      .replace(/\${subscribeUrl}/g, subscribeUrl)
+      .replace(
+        /([^\r\n]*)\$\{directDomain\}([^\r\n]*)(\r?\n)/m,
+        directDomains.map((directDomain) => `$1${directDomain}$2$3`).join(''),
+      )
+    return
+  }
+  ctx.throw(404)
 })
 
 export default router
